fe71a69513851640782f37f4ef1ce6868e27cdf9
Maxime NORVAL <maximen@isimedia.intra>
2020-09-15 08:27:58 +0200

    header works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HeaderService } from './services/header.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loadedComponent: string = 'authors';
+
+  constructor(private headerService: HeaderService) {}
+
+  ngOnInit(): void {
+    this.headerService.loadedComponentSubject.subscribe((component) => {
+      this.loadedComponent = component;
+    });
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { HeaderService } from '../services/header.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
 
+  constructor(private headerService : HeaderService) {}
+
+  onSelectComponent(component : string) {
+    this.headerService.loadedComponentSubject.next(component);  }
+}
