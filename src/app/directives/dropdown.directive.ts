843ecf868e1315ce7f9f2c220205748a8760d770
Maxime NORVAL <maximen@isimedia.intra>
2020-09-16 10:21:39 +0200

    dropdown user works but alignment of items is to correct

--- src/app/directives/dropdown.directive.ts
+++ src/app/directives/dropdown.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen: boolean = false;
-
-  @HostListener('click') toggleOpen(): void {
-    this.isOpen = !this.isOpen;
+  @HostListener('click') toggleDisplay() {
+    this.elementRef.nativeElement
+      .querySelector('.dropdown-menu')
+      .classList.toggle('show');
   }
+
+  constructor(private elementRef: ElementRef) {}
 }
